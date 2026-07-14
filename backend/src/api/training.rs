35e32d53a37b1d403efb6ef25aa9338edc341caf
Sergej Maul <maulser@googlemail.com>
2026-07-14 15:01:05 +0200

    fix: remove dead code allowance

--- backend/src/api/training.rs
+++ backend/src/api/training.rs
@@ -20,19 +20,14 @@ pub struct PaginationParams {
 }
 
 #[derive(Debug, Deserialize)]
-#[allow(dead_code)]
 pub struct StatsFilterParams {
     pub from: Option<String>,
     pub to: Option<String>,
-    pub plan_id: Option<String>,
-    pub exercise_id: Option<String>,
 }
 
 #[derive(Debug, Deserialize)]
-#[allow(dead_code)]
 pub struct ExerciseFilterParams {
     pub equipment: Option<String>,
-    pub muscle: Option<String>,
     pub pattern: Option<String>,
     pub difficulty: Option<String>,
     pub search: Option<String>,
